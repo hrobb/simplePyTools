2e45a5c675e7a32cac8920cd5853764c7af7e951
Hunter Robb <hunter_robb@yahoo.com>
2025-02-20 18:15:51 -0600

    Added title, restructured main menu for clarity

--- gui/views/main_menu.py
+++ gui/views/main_menu.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QGridLayout, QVBoxLayout, QLabel, QPushButton
+from PyQt6.QtWidgets import QWidget, QGridLayout, QVBoxLayout, QLabel, QPushButton, QHBoxLayout
+from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt, pyqtSignal
 
 class MainMenu(QWidget):
@@ -10,25 +11,63 @@ class MainMenu(QWidget):
 		self._init_ui()
 
 	def _init_ui(self):
-		# Create a widget for each script and place in main menu grid
-		layout = QGridLayout()
+		layout = QVBoxLayout(self)
+		self.setLayout(layout)
+		self.setContentsMargins(20, 20, 20, 20)
+
+		layout.addWidget(self._create_title_widget(), 1)
+		layout.addWidget(self._create_grid_widget(), 9)
+		
+	# Create title for main menu
+	def _create_title_widget(self):
+		container = QWidget()
+		container.setMaximumHeight(100)
+		layout = QHBoxLayout(container)
+		layout.setContentsMargins(0, 0, 0, 0)
+
+		logo_label = QLabel()
+		logo = QPixmap("assets/spt.png").scaled(
+			100,
+			100, 
+			Qt.AspectRatioMode.KeepAspectRatio,
+			Qt.TransformationMode.SmoothTransformation
+		)
+		logo_label.setPixmap(logo)
+
+		title_text = QLabel("SimplePyTools")
+		title_text.setStyleSheet('font: 20pt; font-weight:bold')
+
+		layout.addWidget(logo_label)
+		layout.addWidget(title_text)
+		layout.addStretch(1)
+
+		return container
+
+	# Create a main menu grid for holding each script
+	def _create_grid_widget(self):
+		
+		container = QWidget()
+		layout = QGridLayout(container)
+
 		row, col = 0, 0
 
 		for script_id, script_info in self.script_registry.items():
-			container = self._create_function_widget(script_info)
-			layout.addWidget(container, row, col)
+			layout.addWidget(self._create_function_widget(script_info), row, col)
 			col = col + 1
 
-		self.setContentsMargins(50, 100, 50, 100)
-		self.setLayout(layout)
+		return container
 
+	# Create a widget for each script and place in main menu grid
 	def _create_function_widget(self, script_info):
 		container = QWidget()
-		layout = QVBoxLayout()
+		layout = QVBoxLayout(container)
+		layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
 		title = QLabel(script_info.title)
 		title.setStyleSheet('font: 12pt')
+
 		desc = QLabel(script_info.description)
+		
 		button = QPushButton("Launch")
 		button.setMinimumSize(60, 35)
 		button.clicked.connect(lambda: self.selected_tool.emit(script_info))
@@ -37,7 +76,5 @@ class MainMenu(QWidget):
 		layout.addWidget(desc, alignment=Qt.AlignmentFlag.AlignCenter)
 		layout.addSpacing(15)
 		layout.addWidget(button, alignment=Qt.AlignmentFlag.AlignCenter)
-		layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-		container.setLayout(layout)
+		
 		return container
